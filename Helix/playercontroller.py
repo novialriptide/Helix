628d28db4ea70be89023269f00447119a50553a4
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2021-11-26 21:15:38 -0500

    changed player speed once again

--- Helix/playercontroller.py
+++ Helix/playercontroller.py
@@ -2,4 +2,4 @@ from Helix.Sakuya.controllers import BaseController
 
 class PlayerController(BaseController):
     def __init__(self) -> None:
-        super().__init__(0.4)
+        super().__init__(0.3)
