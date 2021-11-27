8c522abd8182eb6dec8bfd3c34e87549044ee3db
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2021-11-27 18:43:50 -0500

    enemy.py cleanup

--- Helix/enemy.py
+++ Helix/enemy.py
@@ -34,42 +34,10 @@ class EnemyEntity(Entity):
         super().__init__(
             controller,
             position,
-            has_collision=has_collision,
-            has_rigidbody=has_rigidbody,
-            scale=scale,
-            particle_systems=particle_systems
+            has_collision = has_collision,
+            has_rigidbody = has_rigidbody,
+            scale = scale,
+            particle_systems = particle_systems,
+            obey_gravity = False
         )
-
-    def move_to(self, target: Vector, distance: int) -> None:
-        """Move towards a target
         
-        Parameters:
-            target: The target's position.
-            distance: The target distance between itself and the target.
-
-        """
-        true_distance = target.x - self.position.x
-
-        # move toward from the left
-        if true_distance > 0 and abs(true_distance) > distance:
-            self.controller.is_moving_right = True
-            self.controller.is_moving_left = False
-            print("move toward from left")
-
-        # move away if too close
-        if true_distance > 0 and abs(true_distance) < distance:
-            self.controller.is_moving_right = False
-            self.controller.is_moving_left = True
-            print(f"move away from left (dis:{distance}, true_dis:{true_distance})")
-
-        # move toward from the right
-        if true_distance < 0 and abs(true_distance) > distance:
-            self.controller.is_moving_right = False
-            self.controller.is_moving_left = True
-            print("move toward from right")
-
-        # move away if too close
-        if true_distance < 0 and abs(true_distance) < distance:
-            self.controller.is_moving_right = True
-            self.controller.is_moving_left = False
-            print(f"move away from right (dis:{distance}, true_dis:{true_distance})")
