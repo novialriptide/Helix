efbde0846a4b6f3073ab946eb71aa2a68b507c1d
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2021-11-29 00:54:55 -0500

    file preparation

---
+++ Helix/effects/backgrounds.py
@@ -0,0 +1,4 @@
+import pygame
+
+def render_tiled_background(surface: pygame.Surface, tile_size: int):
+    rect = surface.get_rect()
---
+++ Helix/loading.py
@@ -0,0 +1,1 @@
+# loading screen that uses parts of the game sprites (4x4) to indicate loading
