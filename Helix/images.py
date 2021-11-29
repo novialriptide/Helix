d1bbc3b4616b1cbdc8f3afca9a7a5c1221a6e22c
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2021-11-28 23:20:29 -0500

    updated enemy1 sprite

--- Helix/images.py
+++ Helix/images.py
@@ -2,14 +2,14 @@ from Helix.Sakuya.tile import split_image
 
 import pygame
 
+projectile_sprites = split_image(
+    pygame.image.load("Helix\sprites\projectiles.png"), 8, 8
+)
+
 player_sprites = split_image(
     pygame.image.load("Helix\sprites\ship.png"), 16, 16
 )
 
 enemy_sprites = split_image(
-    pygame.image.load("Helix\sprites\ship3.png"), 32, 32
-)
-
-projectile_sprites = split_image(
-    pygame.image.load("Helix\sprites\projectiles.png"), 8, 8
+    pygame.image.load("Helix\sprites\enemy1_FINAL.png"), 32, 32
 )
