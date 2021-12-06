a85013e400e0ccfb12e6fc547b5971b2b6b62b37
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2021-12-05 22:47:06 -0500

    Update bullet_test.py

--- Helix/scenes/tests/bullet_test.py
+++ Helix/scenes/tests/bullet_test.py
@@ -4,10 +4,15 @@ This code is licensed under MIT license (see LICENSE for details)
 """
 import sys
 import pygame
+from copy import copy
 
+from Helix.SakuyaEngine.entity import Entity
+from Helix.SakuyaEngine.animation import Animation
 from Helix.SakuyaEngine.math import Vector
 from Helix.SakuyaEngine.scene import Scene
 from Helix.SakuyaEngine.bullets import BulletSpawner, Bullet
+
+from Helix.images import projectile_sprites
 
 class BulletTest(Scene):
     def __init__(self, client):
@@ -21,8 +26,22 @@ class BulletTest(Scene):
         except:
             self.joystick = None
 
+        b_anim = Animation(
+            "b_anim",
+            [projectile_sprites[0], projectile_sprites[1], projectile_sprites[2]],
+            fps = 4
+        )
+
+        e = Entity(None, Vector(100, 100), custom_hitbox_size = Vector(3, 3))
+        b = Bullet(None, 3, (255, 0, 0), 5, custom_hitbox_size = Vector(1, 1), name = "bullet")
+        b.anim_add(copy(b_anim))
+        b.anim_set("b_anim")
+
         self.bullet_spawner_test = BulletSpawner(
-            None, Vector(0, 0), None, iterations = 0, bullets_per_array = 4, total_bullet_arrays = 6, fire_rate = 500
+            e, Vector(0, 0), b, self.entities,
+            iterations = 0, bullets_per_array = 4,
+            total_bullet_arrays = 6, fire_rate = 0,
+            spread_between_bullet_arrays = 57, spread_within_bullet_arrays = 57
         )
 
     def update(self) -> None:
@@ -34,3 +53,7 @@ class BulletTest(Scene):
         self.client.screen.fill((0,0,0))
 
         self.bullet_spawner_test.update(self.client.delta_time)
+        self.advance_frame(self.client.delta_time)
+
+        for e in self.entities:
+            pygame.draw.rect(self.client.screen, (0, 255, 0), e.rect, 1)
