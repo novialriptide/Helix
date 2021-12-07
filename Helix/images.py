231e22421b68d5cdd7312e529374936da46caf95
NovialRiptide <35881688+NovialRiptide@users.noreply.github.com>
2021-12-07 01:06:40 -0500

    added "\\" instead of just "\" in image paths

--- Helix/images.py
+++ Helix/images.py
@@ -7,15 +7,15 @@ import pygame
 from Helix.SakuyaEngine.tile import split_image
 
 projectile_sprites = split_image(
-    pygame.image.load("Helix\sprites\projectiles.png"), 8, 8
+    pygame.image.load("Helix\\sprites\\projectiles.png"), 8, 8
 )
 
 player_sprites = split_image(
-    pygame.image.load("Helix\sprites\ship.png"), 16, 16
+    pygame.image.load("Helix\\sprites\\ship.png"), 16, 16
 )
 
 enemy_sprites = split_image(
-    pygame.image.load("Helix\sprites\enemy1_FINAL.png"), 32, 32
+    pygame.image.load("Helix\\sprites\\ado_idle_anim.png"), 32, 32
 )
 
-pygame_powered_logo = pygame.image.load("Helix\sprites\pygame_powered.png")
+pygame_powered_logo = pygame.image.load("Helix\\sprites\\pygame_powered.png")
