8c0260adee303aaeae4774326920df65b1cab0eb
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2021-12-12 05:01:28 -0500

    Replace SakuyaEngine.Vector with pygame.math.Vector2

--- Helix/scenes/start.py
+++ Helix/scenes/start.py
@@ -7,7 +7,7 @@ import pygame
 
 from Helix.SakuyaEngine.entity import load_entity_json
 from Helix.SakuyaEngine.scene import Scene
-from Helix.SakuyaEngine.math import Vector
+
 from Helix.SakuyaEngine.particles import Particles
 from Helix.SakuyaEngine.waves import load_wave_file
 from Helix.SakuyaEngine.errors import SceneNotActiveError
@@ -34,27 +34,27 @@ class Start(Scene):
 
         self.wave_manager = HelixWaves(30000)
         self.wave_manager.spawn_points = [
-            Vector(int(win_size.x * 1/5), int(win_size.y * 1/4)),
-            Vector(int(win_size.x * 1/3), int(win_size.y * 1/7)),
-            Vector(int(win_size.x * 1/2), int(win_size.y * 1/7)),
-            Vector(int(win_size.x * 2/3), int(win_size.y * 1/7)),
-            Vector(int(win_size.x * 4/5), int(win_size.y * 1/4))
+            pygame.math.Vector2(int(win_size.x * 1/5), int(win_size.y * 1/4)),
+            pygame.math.Vector2(int(win_size.x * 1/3), int(win_size.y * 1/7)),
+            pygame.math.Vector2(int(win_size.x * 1/2), int(win_size.y * 1/7)),
+            pygame.math.Vector2(int(win_size.x * 2/3), int(win_size.y * 1/7)),
+            pygame.math.Vector2(int(win_size.x * 4/5), int(win_size.y * 1/4))
         ]
 
         self.player_entity = load_entity_json("Helix\\data\\entity\\helix.json")
-        self.player_entity.position = Vector(win_size.x/2, win_size.y/2)
+        self.player_entity.position = pygame.math.Vector2(win_size.x/2, win_size.y/2)
         self.player_entity.controller = PlayerController()
         self.player_entity.anim_set("idle_anim")
         player_rect = self.player_entity.rect
         self.player_entity.particle_systems = [
             Particles(
-                Vector(0, 2),
+                pygame.math.Vector2(0, 2),
                 colors = [
                     (249, 199, 63),
                     (255, 224, 70),
                     (255, 78, 65)
                 ],
-                offset = Vector(
+                offset = pygame.math.Vector2(
                     player_rect.width/2,
                     player_rect.height * 1/4
                 ),
@@ -162,7 +162,7 @@ class Start(Scene):
         for b in self.bullets:
             if b.position.y < 0 or b.position.y > self.client.screen.get_height() or b.position.x < 0 or b.position.x > self.client.screen.get_width():
                 b._is_destroyed = True
-            self.client.screen.blit(b.sprite, b.position.to_list())
+            self.client.screen.blit(b.sprite, b.position)
             spotlight(self.client.screen, b.position + b.center_offset, (20, 0, 20), 10)
             #pygame.draw.rect(self.client.screen, (0, 255, 0), b.custom_hitbox, 1)
 
@@ -180,11 +180,11 @@ class Start(Scene):
                             e._is_destroyed = True
 
             # Draw
-            self.client.screen.blit(e.sprite, e.position.to_list())
+            self.client.screen.blit(e.sprite, e.position)
             # TODO: Implement this in Entity
             if e.draw_healthbar:
                 bar_length = e.rect.width * 0.7
-                bar_pos = e.position + e.healthbar_position_offset + e.center_offset - Vector(bar_length / 2 - 1, e.rect.height * (2 / 3))
+                bar_pos = e.position + e.healthbar_position_offset + e.center_offset - pygame.math.Vector2(bar_length / 2 - 1, e.rect.height * (2 / 3))
                 display_hp = (e.healthbar.display_health / e._max_health) * bar_length
                 pygame.draw.rect(self.client.screen, (0, 230, 0), pygame.Rect(
                     bar_pos.x, bar_pos.y, display_hp, 1
@@ -193,7 +193,7 @@ class Start(Scene):
                     bar_pos.x, bar_pos.y + 1, display_hp, 1
                 ))
 
-        # for sp in self.wave_manager.spawn_points: self.client.screen.set_at(sp.to_list(), (255,255,255))
+        # for sp in self.wave_manager.spawn_points: self.client.screen.set_at(sp, (255,255,255))
         # for e in self.entities: pygame.draw.rect(self.client.screen, (0, 255, 0), e.custom_hitbox, 1)
 
         for p in self.particle_systems:
--- main.py
+++ main.py
@@ -4,9 +4,10 @@ This code is licensed under MIT license (see LICENSE for details)
 """
 import ctypes
 import argparse
+import pygame
 
 from Helix.SakuyaEngine.client import Client
-from Helix.SakuyaEngine.math import Vector
+
 from Helix.SakuyaEngine.scene import SceneManager
 
 from Helix.scenes.start import Start
@@ -19,14 +20,14 @@ from Helix.const import player_sprites
 
 # get user's MONITOR RESOLUTION
 user32 = ctypes.windll.user32
-monitor_resolution = Vector(
+monitor_resolution = pygame.math.Vector2(
     user32.GetSystemMetrics(0),
     user32.GetSystemMetrics(1)
 )
 
 HelixClient = Client(
     "Helix",
-    Vector(256, 224),
+    pygame.math.Vector2(256, 224),
     scale_upon_startup = 2,
     window_icon = player_sprites[0]
 )
