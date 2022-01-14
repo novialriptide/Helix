9a2d55f5e7182e85228a9531e022b0fbbc5b4017
Andrew Hong <35881688+NovialRiptide@users.noreply.github.com>
2022-01-14 01:47:05 -0500

    Add secondary controller

--- src/Helix/playercontroller.py
+++ src/Helix/playercontroller.py
@@ -9,3 +9,7 @@ class PlayerController(BaseController):
     def __init__(self) -> None:
         super().__init__()
         self.is_shooting = False
+
+class SecondaryController(BaseController):
+    def __init__(self) -> None:
+        super().__init__()
